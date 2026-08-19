91dba28268657c41ebbaec1572258b927c5ae24b
stuppie <stuppie@gmail.com>
2026-08-19 13:47:06 -0600

    not sure why these imports got deleted for no reason

--- generalresearch/models/morning/question.py
+++ generalresearch/models/morning/question.py
@@ -1,6 +1,6 @@
 import json
 from enum import Enum
-from typing import Any, Literal
+from typing import Any, Literal, Dict, List, Optional
 from uuid import UUID
 
 from pydantic import BaseModel, Field, field_validator, model_validator
