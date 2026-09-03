fe531d691d75193619d43abf22d22599171539fd
Max Nanis <max@g-r-l.com>
2026-09-02 17:46:39 -0700

    3.12 + 3.12 removal from TYPE_CHECK

--- generalresearch/models/network/mtr/parser.py
+++ generalresearch/models/network/mtr/parser.py
@@ -1,10 +1,8 @@
 import json
-from typing import TYPE_CHECKING
+from typing import Any
 
+from generalresearch.models.network.definitions import IPProtocol
 from generalresearch.models.network.mtr.result import MTRResult
-
-if TYPE_CHECKING:
-    from generalresearch.models.network.definitions import IPProtocol
 
 
 def parse_mtr_output(raw: str, port: int, protocol: IPProtocol) -> MTRResult:
@@ -14,7 +12,7 @@ def parse_mtr_output(raw: str, port: int, protocol: IPProtocol) -> MTRResult:
     return MTRResult.model_validate(data)
 
 
-def parse_mtr_raw_output(raw: str) -> dict:
+def parse_mtr_raw_output(raw: str) -> dict[str, Any]:
     data = json.loads(raw)["report"]
     data.update(data.pop("mtr"))
     data["hops"] = data.pop("hubs")
